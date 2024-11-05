ef86d7ca14f8a458d82537b7bdd0185ac7ec9a2e
Afro-Tyche <afrotyche@gmail.com>
2024-11-05 16:29:49 +0100

    üpdated db-setup

--- db-engine/db-setup.sh
+++ db-engine/db-setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Load environment variables from .env file
-source .env
+# Set project root and .env file path
+project_root="$(dirname "$(dirname "$(realpath "$0")")")"
+path_to_env_file="$project_root/.env"
+
+# Check if the .env file exists in the root directory
+if [ ! -f "$path_to_env_file" ]; then
+    echo ".env file not found in the project's root directory: $project_root"
+    exit 1
+fi
+
+# Load the .env file
+export $(grep -v '^#' "$path_to_env_file" | xargs)
 
 # Prompt for MySQL root password
 echo "Enter MySQL root password:"
@@ -14,8 +24,8 @@ mysql -u root -p"$root_password" <<EOF
 CREATE DATABASE IF NOT EXISTS Global_Housing_Data;
 
 -- Create user and grant privileges
-CREATE USER IF NOT EXISTS '$DB_HOST'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON Global_Housing_Data.* TO '$DB_HOST'@'localhost';
+CREATE USER IF NOT EXISTS '$DB_USER'@'$DB_HOST' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON Global_Housing_Data.* TO '$DB_USER'@'$DB_HOST';
 
 -- Apply privileges
 FLUSH PRIVILEGES;
